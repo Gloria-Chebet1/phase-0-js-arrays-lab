659e3dc07c653c25eef7105cf9aea99d94eb467d
Gloria Chebet <gloriachebetkerubo@gmail.com>
2026-04-13 18:13:38 +0300

    done

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,29 @@
 const products=["Laptop", "Phone", "Headphones", "Monitor"];
 function logFirstProduct(products){
-  console.log(products[0])
+  console.log(products[0]);
 }
+logFirstProduct(products);
+
+function addProduct(products, productName){
+  products.push(productName);
+}
+addProduct(products, "Tablet");
+console.log(products);
+
+function updateProductName(products, index, newName){
+  products[index]=newName;
+}
+updateProductName(products, 1, "Smartphone");
+console.log(products);
+
+function removeLastProduct(products){
+  products.pop();
+}
+removeLastProduct(products);
+console.log(products);
+
+
+
 
 
 
